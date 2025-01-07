e61150277e8e583bbf997b5479b5808e4f9e9776
devmehmetakifv <go4webb@gmail.com>
2025-01-07 19:27:04 +0300

    Session management initialized.

---
+++ Libraria/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Libraria.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public DateTime MembershipDate { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
