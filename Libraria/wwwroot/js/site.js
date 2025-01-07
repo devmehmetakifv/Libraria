0d945670098089939aaf81fc19c6dd0f7fa98109
devmehmetakifv <go4webb@gmail.com>
2025-01-07 19:01:19 +0300

    Installing new theme.

--- Libraria/wwwroot/js/site.js
+++ Libraria/wwwroot/js/site.js
@@ -1,4 +1,52 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$('.reply-ticket-btn').click(function () {
+    console.log("Work!");
+    var ticketId = $(this).data('ticket-id');
+    var userNameWhoReplies = $(this).data('user-name');
+    $('#replyTicketId').val(ticketId);
+    $('#userNameWhoReplies').val(userNameWhoReplies);
+    $('#replyTicketModal').modal('show');
+});
 
-// Write your JavaScript code.
+$('#replyTicketForm').submit(function (e) {
+    $('#replyTicketModal').modal('hide');
+});
+
+$('.delete-user-btn').click(function () {
+    var userId = $(this).data('user-id');
+    $('#deleteUserId').val(userId);
+    $('#deleteUserModal').modal('show');
+});
+
+$('#deleteUserForm').submit(function (e) {
+    $('#deleteUserModal').modal('hide');
+});
+
+$('.delete-course-btn').click(function () {
+    var courseId = $(this).data('course-id');
+    $('#deleteCourseId').val(courseId);
+    $('#deleteCourseModal').modal('show');
+});
+
+$('#deleteUserForm').submit(function (e) {
+    $('#deleteUserModal').modal('hide');
+});
+
+$('.delete-program-btn').click(function () {
+    var programId = $(this).data('program-id');
+    $('#deleteProgramId').val(programId);
+    $('#deleteProgramModal').modal('show');
+});
+
+$('#deleteProgramForm').submit(function (e) {
+    $('#deleteProgramModal').modal('hide');
+});
+
+$('.delete-department-btn').click(function () {
+    var departmentId = $(this).data('department-id');
+    $('#deleteDepartmentId').val(departmentId);
+    $('#deleteDepartmentModal').modal('show');
+});
+
+$('#deleteDepartmentForm').submit(function (e) {
+    $('#deleteDepartmentModal').modal('hide');
+})
